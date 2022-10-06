0ac1e588de0a06bc1764808465d8be33846fe6f2
Felipe Goriano <Felipe Goriano@192.168.1.16>
2022-10-05 21:37:14 -0300

    Commiti inicial - configurando BD

---
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/GorisfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.gorisfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorisfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorisfoodApiApplication.class, args);
+	}
+
+}
