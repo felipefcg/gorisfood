88af37401fbf4e2b5d654f7fefde79e8866b19d2
Felipe Goriano <Felipe Goriano@192.168.1.16>
2022-10-14 21:43:18 -0300

    4.15. Customizando as representações XML e JSON com @anotações
    4.15. Customizando as representações XML e JSON com @JsonIgnore,
    @JsonProperty e @JsonRootName

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/domain/model/Cozinha.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/domain/model/Cozinha.java
@@ -5,18 +5,26 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 @Entity
+@JsonRootName("gastronomia")
 public class Cozinha {
 	
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+//	@JsonIgnore
+	@JsonProperty("titulo")
 	private String nome;
 	
 //	@OneToMany(mappedBy = "cozinha")
