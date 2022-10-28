0b33dbed31d4e5dd111a65d79d6108daae05eea8
Felipe Goriano <Felipe Goriano@192.168.1.16>
2022-10-28 07:37:06 -0300

    5.12. Implementando uma consulta dinâmica com JPQL

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,15 @@
 package br.com.felipe.gorisfood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import br.com.felipe.gorisfood.domain.model.Restaurante;
 import br.com.felipe.gorisfood.domain.repository.RestauranteRepositoryCustom;
@@ -19,11 +22,28 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryCustom {
 	
 	@Override
 	public List<Restaurante> consultarPorCozinhaETaxa(String nomeCozinha, BigDecimal taxaInicial, BigDecimal taxaFinal) {
-		var jpql = "from Restaurante where cozinha.nome like :nome and taxaFrete between :taxaInicial and :taxaFinal";
-		return em.createQuery(jpql, Restaurante.class)
-				.setParameter("nome", "%" + nomeCozinha + "%")
-				.setParameter("taxaInicial", taxaInicial)
-				.setParameter("taxaFinal", taxaFinal)
-				.getResultList();
+		var jpql = new StringBuilder("from Restaurante where 1 = 1 ");
+		var parametros = new HashMap<String, Object>();
+		
+//		if(!nomeCozinha.isEmpty()) {
+		if(StringUtils.hasLength(nomeCozinha)) {
+			jpql.append(" and cozinha.nome like :nome ");
+			parametros.put("nome", "%" + nomeCozinha + "%");
+		}
+		
+		if(taxaInicial != null) {
+			jpql.append("and taxaFrete >= :taxaInicial ");
+			parametros.put("taxaInicial", taxaInicial);
+		}
+		
+		if(taxaFinal != null) {
+			jpql.append("and taxaFrete <= :taxaFinal");
+			parametros.put("taxaFinal", taxaFinal);
+		}
+		
+		TypedQuery<Restaurante> query = em.createQuery(jpql.toString(), Restaurante.class);		
+		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		
+		return query.getResultList();
 	}
 }
