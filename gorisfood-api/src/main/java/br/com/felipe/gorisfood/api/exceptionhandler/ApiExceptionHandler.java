313ec6da42f57175feafe9f823c92919b59cbf09
Felipe Goriano <Felipe Goriano@192.168.1.16>
2022-11-19 15:45:07 -0300

    8.13.1 Tratando exceções globais
    8.13 Tratando exceções globais com @ExceptionHandler e
    @RestControllerAdvice

---
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,35 @@
+package br.com.felipe.gorisfood.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.felipe.gorisfood.domain.exception.EntidadeNaoEncontradaException;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public Problema handleCidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+		return Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+	}
+	
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	@ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+	public Problema handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+		
+		return Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(String.format("O tipo de mídia %s não é aceito", e.getContentType()))
+				.build();
+	}
+	
+}
