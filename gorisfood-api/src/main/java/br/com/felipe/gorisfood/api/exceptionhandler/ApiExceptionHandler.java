50b6aaab71635403e863bb8b65e2cd5e753825a8
Felipe Goriano <Felipe Goriano@192.168.1.16>
2022-11-19 16:12:42 -0300

    8.13.2 Tratando exceções globais
    8.13 Tratando exceções globais com @ExceptionHandler e
    @RestControllerAdvice

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/exceptionhandler/ApiExceptionHandler.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,13 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.felipe.gorisfood.domain.exception.EntidadeNaoEncontradaException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class ApiExceptionHandler {
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
@@ -31,5 +32,16 @@ public class ApiExceptionHandler {
 				.mensagem(String.format("O tipo de mídia %s não é aceito", e.getContentType()))
 				.build();
 	}
+	
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+	public Problema handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+		
+		return Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(String.format("O método %s não é suportado para essa URI", e.getMethod()))
+				.build();
+	}
+	
 	
 }
