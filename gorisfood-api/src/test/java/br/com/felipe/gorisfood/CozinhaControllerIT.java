d1e94d64414de633f1384b3e72ab86cf5fde022e
Felipe Goriano <Felipe Goriano@192.168.1.14>
2022-12-05 23:43:31 -0300

    10.8. Implementando Testes de API com REST Assured ...
    10.8. Implementando Testes de API com REST Assured e validando o código
    de status HTTP

---
+++ gorisfood-api/src/test/java/br/com/felipe/gorisfood/CozinhaControllerIT.java
@@ -0,0 +1,34 @@
+package br.com.felipe.gorisfood;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class CozinhaControllerIT {
+
+	@LocalServerPort
+	int port;
+	
+	@Test
+	void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+
+}
