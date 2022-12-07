c458ef8191a5ec98cb2bb5f30b450a9535515d13
Felipe Goriano <Felipe Goriano@192.168.1.14>
2022-12-06 21:57:48 -0300

    10.9. Validando o corpo da resposta HTTP

--- gorisfood-api/src/test/java/br/com/felipe/gorisfood/CozinhaControllerIT.java
+++ gorisfood-api/src/test/java/br/com/felipe/gorisfood/CozinhaControllerIT.java
@@ -2,6 +2,7 @@ package br.com.felipe.gorisfood;
 
 import static io.restassured.RestAssured.given;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -29,6 +30,21 @@ class CozinhaControllerIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	void deve4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", Matchers.hasSize(4))
+			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
 	}
 
 }
