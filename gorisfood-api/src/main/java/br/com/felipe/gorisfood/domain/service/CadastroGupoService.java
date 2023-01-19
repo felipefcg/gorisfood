8d6223c55a1cab5b0e60823e35632c67793f84c4
Felipe Goriano <Felipe Goriano@192.168.1.18>
2023-01-19 06:57:35 -0300

    12.8. Desafio: implementando os endpoints de grupos - Ajuste Delete

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/domain/service/CadastroGupoService.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/domain/service/CadastroGupoService.java
@@ -3,16 +3,20 @@ package br.com.felipe.gorisfood.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.felipe.gorisfood.domain.exception.EntidadeEmUsoExcpetion;
 import br.com.felipe.gorisfood.domain.exception.GrupoNaoEncontradoException;
 import br.com.felipe.gorisfood.domain.model.Grupo;
 import br.com.felipe.gorisfood.domain.repository.GrupoRepository;
 
 @Service
 public class CadastroGupoService {
+	
+	private static final String GRUPO_EM_USO = "Grupo %d não pode ser removida pois está em uso.";
 	
 	@Autowired
 	private GrupoRepository repository;
@@ -34,6 +38,8 @@ public class CadastroGupoService {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new GrupoNaoEncontradoException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoExcpetion(String.format(GRUPO_EM_USO, id));
 		}
 	}
 	
