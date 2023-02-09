36912f8971419cc91b561357248fc93fac38a48a
Felipe Goriano <Felipe Goriano@192.168.1.8>
2023-02-09 08:04:34 -0300

    13.2. Limitando os campos retornados pela API com @JsonFilter do Jackson

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/PedidoController.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/PedidoController.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import br.com.felipe.gorisfood.api.assembler.PedidoRequestDtoDisassembler;
 import br.com.felipe.gorisfood.api.assembler.PedidoResponseDtoAssembler;
@@ -38,9 +44,25 @@ public class PedidoController {
 	@Autowired
 	private PedidoResumidoResponseDtoAssembler pedidoResumidoAssembler;
 	
+//	@GetMapping
+//	public List<PedidoResumidoResponseDTO> listar() {
+//		return pedidoResumidoAssembler.toDtoList(emissaoPedidoService.listar());
+//	}
+	
 	@GetMapping
-	public List<PedidoResumidoResponseDTO> listar() {
-		return pedidoResumidoAssembler.toDtoList(emissaoPedidoService.listar());
+	public MappingJacksonValue listar(@RequestParam(required = false) String[] campos) {
+		List<PedidoResumidoResponseDTO> pedidosDTO = pedidoResumidoAssembler.toDtoList(emissaoPedidoService.listar());
+
+		MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosDTO);
+		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+		filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+		
+		if(ArrayUtils.isNotEmpty(campos)) {
+			filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos));
+		}
+		
+		pedidosWrapper.setFilters(filterProvider);
+		return pedidosWrapper;
 	}
 	
 	@GetMapping("{codigoPedido}")
--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/response/PedidoResumidoResponseDTO.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/response/PedidoResumidoResponseDTO.java
@@ -3,12 +3,15 @@ package br.com.felipe.gorisfood.api.model.response;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
 import br.com.felipe.gorisfood.domain.model.enums.StatusPedido;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonFilter("pedidoFilter")
 public class PedidoResumidoResponseDTO {
 	private String codigo;
 	private BigDecimal subtotal;
