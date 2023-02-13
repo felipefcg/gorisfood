c73e989c7054fbe59574563b0f0a6c9e4cb6c6f9
Felipe Goriano <Felipe Goriano@192.168.1.8>
2023-02-12 23:59:33 -0300

    13.15. Desafio: adicionando os filtros na consulta (minha versão)
    13.15. Desafio: adicionando os filtros na consulta de vendas diárias

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/service/VendaQueryServiceImpl.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,11 +1,15 @@
 package br.com.felipe.gorisfood.infrastructure.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -35,9 +39,27 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 				functionDateDataCriacao, criteriaBuilder.count(root), criteriaBuilder.sum(root.get("valorTotal")));
 		
 		criteriaQuery.select(selection);
+		criteriaQuery.where(criarClausulasWhere(criteriaBuilder, root, filtro));
 		criteriaQuery.groupBy(functionDateDataCriacao);
 		
 		return em.createQuery(criteriaQuery).getResultList();
 	}
 
+	private Predicate[] criarClausulasWhere(CriteriaBuilder builder, Root<Pedido> root, VendaDiariaFilter filtro) {
+		var predicates = new ArrayList<Predicate>();
+		
+		if(filtro.getRestauranteId() !=  null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		if(filtro.getDataCriacaoInicio() !=  null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+		}
+
+		if(filtro.getDataCriacaoFim() !=  null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+		}
+		
+		return predicates.toArray(new Predicate[0]);
+	}
 }
