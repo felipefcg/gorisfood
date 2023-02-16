88bb6e51af947762e5fb557c9a359d18f8df5d6a
Felipe Goriano <Felipe Goriano@192.168.1.8>
2023-02-16 08:35:33 -0300

    13.20.1 *Versionando o JRXML e compilando em tempo de execução
    13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando
    bytes do PDF

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/service/report/PdfVendaReportServiceImpl.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/service/report/PdfVendaReportServiceImpl.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Service;
 import br.com.felipe.gorisfood.domain.filter.VendaDiariaFilter;
 import br.com.felipe.gorisfood.domain.service.VendaQueryService;
 import br.com.felipe.gorisfood.domain.service.VendaReportService;
+import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
@@ -22,13 +24,17 @@ public class PdfVendaReportServiceImpl implements VendaReportService {
 	@Override
 	public byte[] consultaVendasDiaria(VendaDiariaFilter filtro, String timeOffset) {
 		try {
-			var jasperInputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+//			var jasperInputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+			var jasperJrxmlInputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jrxml");
+			
 			var parametros = new HashMap<String, Object>();
 			parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
 			
 			var dataSource = new JRBeanCollectionDataSource(vendaQueryService.consultaVendasDiaria(filtro, timeOffset));
-		
-			var jasperPrint = JasperFillManager.fillReport(jasperInputStream, parametros, dataSource);
+			JasperReport compileReport = JasperCompileManager.compileReport(jasperJrxmlInputStream);
+			
+//			var jasperPrint = JasperFillManager.fillReport(jasperInputStream, parametros, dataSource);
+			var jasperPrint = JasperFillManager.fillReport(compileReport, parametros, dataSource);
 			return JasperExportManager.exportReportToPdf(jasperPrint);
 		} catch (Exception e) {
 			throw new ReportException("Não foi possível emitir relatório de vendas diárias");
