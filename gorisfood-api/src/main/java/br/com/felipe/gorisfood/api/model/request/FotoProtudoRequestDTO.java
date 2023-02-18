2b93a4ce1bc312eeec1fa4bca90e8ed57e5c0480
Felipe Goriano <Felipe Goriano@192.168.1.8>
2023-02-17 21:24:21 -0300

    14.4. Desafio: Validando o content type do arquivo (minha implementação)
    14.4. Desafio: Validando o content type do arquivo

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/request/FotoProtudoRequestDTO.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/request/FotoProtudoRequestDTO.java
@@ -3,8 +3,10 @@ package br.com.felipe.gorisfood.api.model.request;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import br.com.felipe.gorisfood.core.validation.FileContentType;
 import br.com.felipe.gorisfood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +16,8 @@ import lombok.Setter;
 public class FotoProtudoRequestDTO {
 	
 	@NotNull
-	@FileSize(max = "20KB")
+	@FileSize(max = "200KB")
+	@FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
 	private MultipartFile arquivo;
 	
 	@NotBlank	
---
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,31 @@
+package br.com.felipe.gorisfood.core.validation;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+	private List<String> allowed;
+	
+	@Override
+	public void initialize(FileContentType constraintAnnotation) {
+		this.allowed = Arrays.asList(constraintAnnotation.allowed());
+	}
+	
+	@Override
+	public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
+			
+		return isContentTypeDesejado(value);
+	}
+
+	private boolean isContentTypeDesejado(MultipartFile value) {
+		
+		return value == null ||  allowed.contains(value.getContentType());
+	}
+
+}
