7321daa31825e1c985ef476246641fea0749ff9d
Felipe Goriano <Felipe Goriano@192.168.1.2>
2023-02-27 07:47:02 -0300

    14.16. Corrigindo exception handler de media type não aceita

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/exceptionhandler/ApiExceptionHandler.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/exceptionhandler/ApiExceptionHandler.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -198,6 +199,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		}
 		
 		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return ResponseEntity.status(status).headers(headers).build();
 	}
 	
 	private ResponseEntity<Object> handleMethosArgumentTypeMismatch(MethodArgumentTypeMismatchException ex, HttpHeaders headers,
