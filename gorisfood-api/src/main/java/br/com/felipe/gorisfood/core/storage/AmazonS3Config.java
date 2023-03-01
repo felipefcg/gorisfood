d5b781a90a52dd2793e46f83a705658c50d3b28e
Felipe Goriano <Felipe Goriano@192.168.1.2>
2023-03-01 07:55:36 -0300

    14.22. Definindo bean do client da Amazon S3 e configurando credenciais

---
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/storage/AmazonS3Config.java
@@ -0,0 +1,29 @@
+package br.com.felipe.gorisfood.core.storage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AmazonS3Config {
+
+	@Autowired
+	private StorageProperties storageProperties;
+	
+	@Bean
+	public AmazonS3 amazonS3() {
+		var basicAwsCredentials = new BasicAWSCredentials(
+				storageProperties.getS3().getIdChaveAcesso(), 
+				storageProperties.getS3().getChaveAcessoSecreta());
+		
+		return AmazonS3ClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(basicAwsCredentials))
+				.withRegion(storageProperties.getS3().getRegiao())
+				.build();
+	}
+}
--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/service/storage/S3FotoStorageService.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,13 +2,19 @@ package br.com.felipe.gorisfood.infrastructure.service.storage;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.s3.AmazonS3;
 
 import br.com.felipe.gorisfood.domain.service.FotoStorageService;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService {
 
+	@Autowired
+	private AmazonS3 amazonS3;
+	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
 
