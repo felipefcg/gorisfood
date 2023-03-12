1ccc19be5ec0d29fbb887d7e030c2fb3b8f27eda
Felipe Goriano <Felipe Goriano@Felipe-Windows>
2023-03-11 22:53:37 -0300

    16.6. Habilitando CORS globalmente no projeto da API

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/RestauranteController.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/RestauranteController.java
@@ -7,11 +7,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/web/WebConfig.java
@@ -0,0 +1,19 @@
+package br.com.felipe.gorisfood.core.web;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+			.addMapping("/**")
+//			.allowedOrigins("*")
+//			.allowedMethods("GET", "PUT", "POST")
+//			.maxAge(30)
+			;
+	}
+}
