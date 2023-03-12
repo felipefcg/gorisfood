9de6fbdf674d26c3bb8906cd2e80365e6423231e
Felipe Goriano <Felipe Goriano@Felipe-Windows>
2023-03-12 12:54:13 -0300

    16.7. Desafio: implementando uma requisição GET com JavaScript

--- client-js/client.js
+++ client-js/client.js
@@ -20,4 +20,32 @@ function fecharRestaurante() {
     });
 }
 
-$("#botao").click(consultarRestaurantes)
+function consultarFormasPagamento() {
+    
+    $.ajax({
+        url: "http://localhost:8080/formas-pagamento",
+        type: "get",
+
+        success: function(response){
+            preencherTabela(response);
+        }
+        
+    });
+}
+
+function preencherTabela(formasPagamento) {
+    $("#tabela tbody tr").remove();
+
+    $.each(formasPagamento, function (i, formaPagamento) {
+        var linha = $("<tr>");
+
+        linha.append (
+            $("<td>").text(formaPagamento.id),
+            $("<td>").text(formaPagamento.descricao)
+        );
+
+        linha.appendTo("#tabela");
+    });
+}
+
+$("#btn-consultar").click(consultarFormasPagamento);
