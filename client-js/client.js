1f163cc8fc49cfbcbe7c9b06cb48c938e2fa3db5
Felipe Goriano <Felipe Goriano@192.168.1.2>
2023-03-13 07:25:38 -0300

    16.9. Desafio: implementando uma requisição DELETE com JavaScript

--- client-js/client.js
+++ client-js/client.js
@@ -61,20 +61,47 @@ function cadastrarFormaPagamento() {
     
 }
 
+function exluirFormaPagamento(formaPagamento) {
+    $.ajax({
+        url: `http://localhost:8080/formas-pagamento/${formaPagamento.id}`,
+        type: "delete",
+        success: function(response) {
+            alert("Forma de pagamento removida!");
+            consultarFormasPagamento();
+        },
+        error: function(error) {
+            if (error.status >= 400 && error.status <= 499) {
+                var problema = error.responseJSON;
+                alert(problema.mensagemUsuario);
+            } else {
+                alert("Erro ao cadastrar forma de pagamento!");
+            }
+        }
+    });
+}
+
 function preencherTabela(formasPagamento) {
     $("#tabela tbody tr").remove();
 
     $.each(formasPagamento, function (i, formaPagamento) {
         var linha = $("<tr>");
+        var linkAcao = $("<a href='#'>")
+                        .text("Excluir")
+                        .click(function(event){
+                            event.preventDefault();
+                            exluirFormaPagamento(formaPagamento);
+                        });
 
         linha.append (
             $("<td>").text(formaPagamento.id),
-            $("<td>").text(formaPagamento.descricao)
+            $("<td>").text(formaPagamento.descricao),
+            $("<td>").append(linkAcao)
         );
 
         linha.appendTo("#tabela");
     });
 }
+
 
 
 
