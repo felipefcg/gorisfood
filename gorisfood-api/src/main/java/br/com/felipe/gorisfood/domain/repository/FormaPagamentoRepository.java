0d60a1f4084544577149057af0290b61db9ed402
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-18 07:30:38 -0300

    17.8. Entendendo e preparando a implementação de Deep ETags

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/domain/repository/FormaPagamentoRepository.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/domain/repository/FormaPagamentoRepository.java
@@ -1,11 +1,16 @@
 package br.com.felipe.gorisfood.domain.repository;
 
-import java.util.List;
+import java.time.OffsetDateTime;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.felipe.gorisfood.domain.model.FormaPagamento;
 
 @Repository
-public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {}
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+	
+	@Query("select max(dataAtualizacao) from FormaPagamento")
+	OffsetDateTime getDataUltimaAtualizacao();
+}
