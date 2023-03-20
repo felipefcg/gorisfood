9eb8b7bef78fc2b410b5d617709ded7bd92524b5
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-20 07:51:06 -0300

    18.3. Gerando a definição OpenAPI em JSON com SpringFox

---
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/openapi/SpringFoxConfig.java
@@ -0,0 +1,24 @@
+package br.com.felipe.gorisfood.core.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@EnableWebMvc
+@Configuration
+@EnableOpenApi
+public class SpringFoxConfig {
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+					.apis(RequestHandlerSelectors.any())
+					.build();
+	}
+}
