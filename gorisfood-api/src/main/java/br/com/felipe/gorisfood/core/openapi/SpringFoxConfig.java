e62480f38bd340a45f6fe214b61b3ec17678b478
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-20 21:30:19 -0300

    18.5. Selecionando os endpoints da API para gerar a documentação

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/openapi/SpringFoxConfig.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/openapi/SpringFoxConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import io.swagger.v3.oas.models.Paths;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.spi.DocumentationType;
@@ -17,7 +19,9 @@ public class SpringFoxConfig {
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.OAS_30)
 				.select()
-					.apis(RequestHandlerSelectors.any())
+					.apis(RequestHandlerSelectors.basePackage("br.com.felipe.gorisfood.api"))
+					.paths(PathSelectors.any())
+//					.paths(PathSelectors.ant("/restaurantes/*"))
 					.build();
 	}
 }
