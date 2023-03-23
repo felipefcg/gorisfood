52b6b5f6d69ed7f21462921d7476608c251fcfd0
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-23 06:57:25 -0300

    18.10. Descrevendo modelos de representações e suas propriedades

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/request/CidadeRequestDTO.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/request/CidadeRequestDTO.java
@@ -4,13 +4,15 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CidadeRequestDTO {
-	
+
+	@ApiModelProperty(example = "Uberlândia")
 	@NotBlank
 	private String nome;
 	
--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/request/EstadoIdRequestDTO.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/request/EstadoIdRequestDTO.java
@@ -2,6 +2,7 @@ package br.com.felipe.gorisfood.api.model.request;
 
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 public class EstadoIdRequestDTO {
 	
+	@ApiModelProperty(example = "1")
 	@NotNull
 	private Long id;
 }
