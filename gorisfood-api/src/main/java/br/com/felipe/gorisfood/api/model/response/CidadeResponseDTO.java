b5099ae1c301494f354cdc91b1fc41bb52e0135d
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-23 06:57:25 -0300

    18.9. Descrevendo parâmetros de entrada na documentação

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/response/CidadeResponseDTO.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/response/CidadeResponseDTO.java
@@ -1,10 +1,19 @@
 package br.com.felipe.gorisfood.api.model.response;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+//@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Data
 public class CidadeResponseDTO {
+	
+//	@ApiModelProperty(value = "ID da cidade", example = "1")
+	@ApiModelProperty(example = "1")
 	private Long id;
+	
+	@ApiModelProperty(example = "Uberlândia")
 	private String nome;
+	
 	private EstadoResponseDTO estado;
 }
--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/response/EstadoResponseDTO.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/model/response/EstadoResponseDTO.java
@@ -1,10 +1,17 @@
 package br.com.felipe.gorisfood.api.model.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class EstadoResponseDTO {
+	
+	@ApiModelProperty(example = "1")
 	private Long id;
+	
+	@ApiModelProperty(example = "MG")
 	private String uf;
+	
+	@ApiModelProperty(example = "Minas Gerais")
 	private String nome;
 }
