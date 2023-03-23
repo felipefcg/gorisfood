e26dc7a3992f9660a531d4f360bb27031e26015f
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-23 08:19:55 -0300

    18.12. Descrevendo códigos de status de respostas de forma global

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/openapi/SpringFoxConfig.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/core/openapi/SpringFoxConfig.java
@@ -1,19 +1,21 @@
 package br.com.felipe.gorisfood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
-import io.swagger.v3.oas.models.Paths;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.bean.validators.plugins.schema.NotBlankAnnotationPlugin;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -31,8 +33,24 @@ public class SpringFoxConfig {
 					.paths(PathSelectors.any())
 //					.paths(PathSelectors.ant("/restaurantes/*"))
 					.build()
+				.useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
 				.tags(new Tag("Cidades", "Gerencia as cidades"))
 				.apiInfo(apiInfo());				
+	}
+
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(
+					getResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Erro interno do servidor"),
+					getResponseMessage(HttpStatus.NOT_ACCEPTABLE, "Recurso não possui representação que poderia ser aceita pelo consumidor")
+				);
+	}
+
+	private Response getResponseMessage(HttpStatus httpStatus, String message) {
+		return new ResponseBuilder()
+				.code(String.valueOf(httpStatus.value()))
+				.description(message)
+				.build();
 	}
 
 	private ApiInfo apiInfo() {
