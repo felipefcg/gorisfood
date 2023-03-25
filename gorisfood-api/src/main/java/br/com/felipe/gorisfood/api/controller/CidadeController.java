9fec89d3eb220c535dc3f376f3de203363f9621a
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-25 17:47:11 -0300

    18.17. Desacoplando anotações do Swagger dos controladores (corrigido)

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/CidadeController.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/CidadeController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.felipe.gorisfood.api.assembler.CidadeRequestDtoDesassembler;
 import br.com.felipe.gorisfood.api.assembler.CidadeResponseDtoAssembler;
+import br.com.felipe.gorisfood.api.controller.openapi.CidadeControllerOpenApi;
 import br.com.felipe.gorisfood.api.exceptionhandler.Problema;
 import br.com.felipe.gorisfood.api.exceptionhandler.TipoProblema;
 import br.com.felipe.gorisfood.api.model.request.CidadeRequestDTO;
@@ -28,21 +29,13 @@ import br.com.felipe.gorisfood.api.model.response.CidadeResponseDTO;
 import br.com.felipe.gorisfood.domain.exception.EstadoNaoEncontradoException;
 import br.com.felipe.gorisfood.domain.model.Cidade;
 import br.com.felipe.gorisfood.domain.service.CadastroCidadeService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 
-@Api(tags = "Cidades")
 @RestController
 @RequestMapping(value = "cidades",
 				consumes = MediaType.APPLICATION_JSON_VALUE,
 				produces = MediaType.APPLICATION_JSON_VALUE)
-public class CidadeController {
+public class CidadeController implements CidadeControllerOpenApi {
 
 	@Autowired
 	private CadastroCidadeService service;
@@ -53,74 +46,35 @@ public class CidadeController {
 	@Autowired
 	private CidadeRequestDtoDesassembler desassembler;
 	
-	@ApiOperation("Lista as cidades")
 	@GetMapping(consumes = MediaType.ALL_VALUE)
 	public List<CidadeResponseDTO> listar() {
 		return assembler.toDtoList(service.listar());
 	}
 
-	@ApiOperation("Busca uma cidade por ID")
-	@ApiResponses({
-		@ApiResponse(responseCode = "400", description = "ID da cidade inválido",  
-					content = {@Content(schema = @Schema(implementation = Problema.class),
-										mediaType = MediaType.APPLICATION_JSON_VALUE)}
-		),
-		@ApiResponse(responseCode = "404", description = "Cidade não encontrada", 
-					 content = {@Content(schema = @Schema(implementation = Problema.class), 
-					 					 mediaType = MediaType.APPLICATION_JSON_VALUE)}
-		)
-	})
 	@GetMapping(value = "{id}", consumes = MediaType.ALL_VALUE)
-	public CidadeResponseDTO buscar(@ApiParam(value = "ID de uma cidade", example = "1" ) @PathVariable Long id) {
+	public CidadeResponseDTO buscar(@PathVariable Long id) {
 		return assembler.toDto(service.buscar(id));
 	}
 	
-	@ApiOperation("Cadastra uma cidade")
-	@ApiResponses({
-		@ApiResponse(responseCode = "201", description = "Cidade cadastrada"),
-		@ApiResponse(responseCode = "422", description = "ID do estado não cadastrado", 
-					 content = {@Content(schema = @Schema(implementation = Problema.class), 
-					 					 mediaType = MediaType.APPLICATION_JSON_VALUE)}
-		)
-	})
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public CidadeResponseDTO criar(@ApiParam(value = "Representação de uma nova cidade") @RequestBody @Valid CidadeRequestDTO cidadeDTO) {
+	public CidadeResponseDTO criar(@RequestBody @Valid CidadeRequestDTO cidadeDTO) {
 		Cidade cidade = desassembler.toModel(cidadeDTO);
 		return  assembler.toDto(service.salvar(cidade));
 	}
 	
-	@ApiOperation("Atualiza uma cidade por ID")
-	@ApiResponses({
-		@ApiResponse(responseCode = "200", description = "Cidade atualizada"),
-		@ApiResponse(responseCode = "404", description = "Cidade não encontrada", 
-					content = {@Content(schema = @Schema(implementation = Problema.class), 
-							   mediaType = MediaType.APPLICATION_JSON_VALUE)}
-		),
-		@ApiResponse(responseCode = "422", description = "ID do estado não cadastrado", 
-					 content = {@Content(schema = @Schema(implementation = Problema.class), 
-					 					 mediaType = MediaType.APPLICATION_JSON_VALUE)}
-		)
-	})
+	
 	@PutMapping("{id}")
-	public CidadeResponseDTO alterar(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long id, 
+	public CidadeResponseDTO alterar(@PathVariable Long id, 
 									 @RequestBody @Valid CidadeRequestDTO cidadeDTO) {
 		Cidade cidadeAtual = service.buscar(id);
 		desassembler.copyDtoToModel(cidadeDTO, cidadeAtual);
 		return assembler.toDto(service.alterar(cidadeAtual));
 	}
 	
-	@ApiOperation(value = "Exclui uma cidade por ID")
-	@ApiResponses({
-		@ApiResponse(responseCode = "204", description = "Cidade excluida"),
-		@ApiResponse(responseCode = "404", description = "Cidade não encontrada", 
-					 content = {@Content(schema = @Schema(implementation = Problema.class), 
-										 mediaType = MediaType.APPLICATION_JSON_VALUE)}
-		)
-	})
 	@DeleteMapping(value = "{id}", consumes = MediaType.ALL_VALUE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public ResponseEntity<Void> remover(@ApiParam(value = "ID de uma cidade", example = "1") @PathVariable Long id) {
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
 			service.remover(id);
 			return ResponseEntity.noContent().build();
 	}
--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/openapi/CidadeControllerOpenApi.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/controller/openapi/CidadeControllerOpenApi.java
@@ -2,32 +2,12 @@ package br.com.felipe.gorisfood.api.controller.openapi;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
-import br.com.felipe.gorisfood.api.assembler.CidadeRequestDtoDesassembler;
-import br.com.felipe.gorisfood.api.assembler.CidadeResponseDtoAssembler;
 import br.com.felipe.gorisfood.api.exceptionhandler.Problema;
-import br.com.felipe.gorisfood.api.exceptionhandler.TipoProblema;
 import br.com.felipe.gorisfood.api.model.request.CidadeRequestDTO;
 import br.com.felipe.gorisfood.api.model.response.CidadeResponseDTO;
-import br.com.felipe.gorisfood.domain.exception.EstadoNaoEncontradoException;
-import br.com.felipe.gorisfood.domain.model.Cidade;
-import br.com.felipe.gorisfood.domain.service.CadastroCidadeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -89,5 +69,4 @@ public interface CidadeControllerOpenApi {
 		)
 	})
 	public ResponseEntity<Void> remover(@ApiParam(value = "ID de uma cidade", example = "1") Long id);
-	
 }
