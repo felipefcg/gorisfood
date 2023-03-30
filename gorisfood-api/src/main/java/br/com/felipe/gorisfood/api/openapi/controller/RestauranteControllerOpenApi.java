d9b71bfef05eafdbb88dc448bd03ade4077b87a0
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-03-30 07:20:39 -0300

    18.28. Descrevendo parâmetros de projeções em endpoints de consultas

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/openapi/controller/RestauranteControllerOpenApi.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/openapi/controller/RestauranteControllerOpenApi.java
@@ -1,10 +1,22 @@
 package br.com.felipe.gorisfood.api.openapi.controller;
 
+import java.util.List;
+
+import br.com.felipe.gorisfood.api.model.response.RestauranteResponseDTO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Api(tags = "Restaurantes")
 @Tag(name= "Restaurantes", description = "Endpoints de acesso às informações relacionadas ao restaurante")
 public interface RestauranteControllerOpenApi {
 
+	@ApiOperation(value = "Listar restaurantes")
+	@ApiImplicitParam(value = "Nome da projeção de restaurantes", 
+						name = "projecao", paramType = "query", dataTypeClass = String.class, allowableValues = "apenas-nome")
+	public List<RestauranteResponseDTO> listar();
+	
+	@ApiOperation(value = "Listar restaurantes", hidden = true)
+	public List<RestauranteResponseDTO> listarApenasNome();
 }
