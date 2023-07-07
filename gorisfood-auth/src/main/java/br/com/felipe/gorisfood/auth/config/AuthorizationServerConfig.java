af3ee4d55142edb32515e734e8d3bba8b40a2be6
Felipe Goriano <Felipe Goriano@192.168.1.23>
2023-07-07 07:34:25 -0300

    22.10 *Config endpoint de introspecção de tokens no Authorization Server
    22.10. Configurando o endpoint de introspecção de tokens no
    Authorization Server

--- gorisfood-auth/src/main/java/br/com/felipe/gorisfood/auth/config/AuthorizationServerConfig.java
+++ gorisfood-auth/src/main/java/br/com/felipe/gorisfood/auth/config/AuthorizationServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -33,6 +34,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				;
 	}
 	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("isAuthenticated()");
+//		security.checkTokenAccess("permitAll()");
+		
+	}
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager);
