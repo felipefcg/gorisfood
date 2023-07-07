3405453cf7c603792355ffcfb1a1b28f6cd7f111
Felipe Goriano <Felipe Goriano@192.168.1.23>
2023-07-06 23:35:58 -0300

    22.9 * Authorization Server com Password Credentials e Opaque Tokens
    22.9. Configurando o fluxo Authorization Server com Password Credentials
    e Opaque Tokens

---
+++ gorisfood-auth/src/main/java/br/com/felipe/gorisfood/auth/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package br.com.felipe.gorisfood.auth.config;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+	
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUser() {
+		var users = new ArrayList<UserDetails>();
+		users.add(User.withDefaultPasswordEncoder()
+				.username("felipe")
+				.password("123")
+				.roles("ADMIN")
+				.build());
+		
+		users.add(User.withUsername("goris")
+					.password(passwordEncoder().encode("123"))
+					.roles("ADMIN")
+					.build());
+		return new InMemoryUserDetailsManager(users);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean 
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+}
