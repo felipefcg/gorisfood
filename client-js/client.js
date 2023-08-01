d2d72f6588b9da282bbe38ddd134d547a5fd6052
Felipe Goriano <Felipe Goriano@192.168.1.23>
2023-08-01 14:54:30 -0300

    22.26. Testando um client JavaScript com PKCE e Authorization Code

--- client-js/client.js
+++ client-js/client.js
@@ -1,109 +1,112 @@
-function consultarRestaurantes() {
+const config = {
+    clientId: "foodanalytics",
+    authorizeUrl: "http://auth.algafood.local:8081/oauth/authorize",
+    tokenUrl: "http://auth.algafood.local:8081/oauth/token",
+    callbackUrl: "http://www.foodanalytics.local:8082",
+    cozinhasUrl: "http://api.algafood.local:8080/v1/cozinhas"
+};
+
+let accessToken = "";
+
+function generateCodeVerifier() {
+    let codeVerifier = generateRandomString(128);
+    localStorage.setItem("codeVerifier", codeVerifier);
+
+    return codeVerifier;
+}
+
+function generateRandomString(length) {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+
+function generateCodeChallenge(codeVerifier) {
+    return base64URL(CryptoJS.SHA256(codeVerifier));
+}
+
+function getCodeVerifier() {
+    return localStorage.getItem("codeVerifier");
+}
+
+function base64URL(string) {
+    return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+}
+
+function consultar() {
+    alert("Consultando recurso com access token " + accessToken);
+
     $.ajax({
-        url: "http://localhost:8080/restaurantes",
+        url: config.cozinhasUrl,
         type: "get",
 
-        success: function (response) {
-            $("#conteudo").text(JSON.stringify(response));
-        }
-    });
-}
-
-function fecharRestaurante() {
-    $.ajax({
-        url: "http://localhost:8080/restaurantes/1/fechamento",
-        type: "put",
-
-        success: function () {
-            alert('Restaurante fechado com sucesso')
-        }
-    });
-}
-
-function consultarFormasPagamento() {
-    
-    $.ajax({
-        url: "http://localhost:8080/formas-pagamento",
-        type: "get",
-
-        success: function(response){
-            preencherTabela(response);
-        }
-        
-    });
-}
-
-function cadastrarFormaPagamento() {
-    var formaPagamentoJson = JSON.stringify({
-        "descricao": $("#campo-descricao").val()
-    });
-      
-    $.ajax({
-        url: "http://localhost:8080/formas-pagamento",
-        type: "post",
-        data: formaPagamentoJson,
-        contentType: "application/json",
-
-        success: function (response) {
-            alert("Forma pagamento adicionadas!");
-            consultarFormasPagamento();
+        beforeSend: function (request) {
+            request.setRequestHeader("Authorization", "Bearer " + accessToken);
         },
+
+        success: function (response) {
+            var json = JSON.stringify(response);
+            $("#resultado").text(json);
+        },
+
         error: function (error) {
-            if (error.status == 400) {
-                var problema = error.responseJSON;
-                alert(problema.mensagemUsuario);
-            } else {
-                alert("Erro ao cadastrar forma de pagamento!");
-            }
+            alert("Erro ao consultar recurso");
         }
-        
     });
-    
 }
 
-function exluirFormaPagamento(formaPagamento) {
+function gerarAccessToken(code) {
+    alert("Gerando access token com code " + code);
+
+    let codeVerifier = getCodeVerifier();
+
+    let params = new URLSearchParams();
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", config.callbackUrl);
+    params.append("client_id", config.clientId);
+    params.append("code_verifier", codeVerifier);
+
     $.ajax({
-        url: `http://localhost:8080/formas-pagamento/${formaPagamento.id}`,
-        type: "delete",
-        success: function(response) {
-            alert("Forma de pagamento removida!");
-            consultarFormasPagamento();
+        url: config.tokenUrl,
+        type: "post",
+        data: params.toString(),
+        contentType: "application/x-www-form-urlencoded",
+
+        success: function (response) {
+            accessToken = response.access_token;
+
+            alert("Access token gerado: " + accessToken);
         },
-        error: function(error) {
-            if (error.status >= 400 && error.status <= 499) {
-                var problema = error.responseJSON;
-                alert(problema.mensagemUsuario);
-            } else {
-                alert("Erro ao cadastrar forma de pagamento!");
-            }
+
+        error: function (error) {
+            alert("Erro ao gerar access key");
         }
     });
 }
 
-function preencherTabela(formasPagamento) {
-    $("#tabela tbody tr").remove();
+function login() {
+    let codeVerifier = generateCodeVerifier();
+    let codeChallenge = generateCodeChallenge(codeVerifier);
 
-    $.each(formasPagamento, function (i, formaPagamento) {
-        var linha = $("<tr>");
-        var linkAcao = $("<a href='#'>")
-                        .text("Excluir")
-                        .click(function(event){
-                            event.preventDefault();
-                            exluirFormaPagamento(formaPagamento);
-                        });
-
-        linha.append (
-            $("<td>").text(formaPagamento.id),
-            $("<td>").text(formaPagamento.descricao),
-            $("<td>").append(linkAcao)
-        );
-
-        linha.appendTo("#tabela");
-    });
+    window.location.href = `${config.authorizeUrl}?response_type=code&client_id=${config.clientId}&redirect_uri=${config.callbackUrl}&code_challenge_method=s256&code_challenge=${codeChallenge}`;
 }
 
+$(document).ready(function () {
+    let params = new URLSearchParams(window.location.search);
 
+    let code = params.get("code");
 
+    if (code) {
+        // window.history.replaceState(null, null, "/");
+        gerarAccessToken(code);
+    }
+});
 
-$("#btn-consultar").click(consultarFormasPagamento);
-$("#btn-cadastrar").click(cadastrarFormaPagamento);
+$("#btn-consultar").click(consultar);
+$("#btn-login").click(login);
