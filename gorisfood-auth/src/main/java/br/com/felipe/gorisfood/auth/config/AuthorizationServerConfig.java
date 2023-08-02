9489d7003f69cd6f464f4fcab91c157ff2d924c5
Felipe Goriano <Felipe Goriano@192.168.1.23>
2023-08-02 12:47:33 -0300

    23.5. *Gerando JWT com chave simétrica no Authorization Server
    23.5. Gerando JWT com chave simétrica (HMAC SHA-256) no Authorization
    Server

--- gorisfood-auth/src/main/java/br/com/felipe/gorisfood/auth/config/AuthorizationServerConfig.java
+++ gorisfood-auth/src/main/java/br/com/felipe/gorisfood/auth/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.com.felipe.gorisfood.auth.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,7 +17,9 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -34,9 +37,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private InMemoryUserDetailsManager userDetailsService;
-	
-	@Autowired
-	private RedisConnectionFactory redisConnectionFactory; 
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -87,13 +87,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.authenticationManager(authenticationManager)
 			.userDetailsService(userDetailsService)
 			.reuseRefreshTokens(false)
+			.accessTokenConverter(jwtAccessTokenConverter())
 			.tokenGranter(tokenGranter(endpoints))
-			.tokenStore(tokenStore())
+			
 		;
 	}
 
-	private TokenStore tokenStore() {
-		return new RedisTokenStore(redisConnectionFactory);
+	@Bean
+	JwtAccessTokenConverter jwtAccessTokenConverter() {
+		var jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("gorisfood");
+		return jwtAccessTokenConverter;
 	}
 
 	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
