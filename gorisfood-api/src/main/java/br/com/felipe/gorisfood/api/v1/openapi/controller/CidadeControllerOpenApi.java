698b00de4e5fe52263fceabf5fb776329f082bdc
Felipe Goriano <Felipe Goriano@host.docker.internal>
2023-09-04 21:22:29 -0300

    26.7. Descrevendo as operações de endpoints na documentação

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -4,6 +4,7 @@ import org.springframework.hateoas.CollectionModel;
 
 import br.com.felipe.gorisfood.api.v1.model.request.CidadeRequestDTO;
 import br.com.felipe.gorisfood.api.v1.model.response.CidadeResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -11,13 +12,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Cidades")
 public interface CidadeControllerOpenApi {
 	
+	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeResponseDTO> listar();
 
+	@Operation(summary = "Busca uma cidade por Od")
 	CidadeResponseDTO buscar(Long id);
 	
+	@Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade, " +
+			"necessita de um estado e um nome válido")
 	CidadeResponseDTO criar(CidadeRequestDTO cidadeDTO);
 	
+	@Operation(summary = "Atualiza uma cidade")
 	CidadeResponseDTO alterar(Long id, CidadeRequestDTO cidadeDTO);
 	
+	@Operation(summary = "Exclui uma cidade")
 	void remover(Long id);
 }
