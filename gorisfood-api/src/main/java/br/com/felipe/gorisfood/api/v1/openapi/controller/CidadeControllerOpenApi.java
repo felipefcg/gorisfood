46fa1db4c1756eab7ef7a4a7df73150a3e54db4a
Felipe Goriano <Felipe Goriano@host.docker.internal>
2023-09-10 12:48:20 -0300

    26.12* Desc códigos de status de respostas em endpoints específicos
    26.12. Descrevendo códigos de status de respostas em endpoints
    específicos

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -1,12 +1,17 @@
 package br.com.felipe.gorisfood.api.v1.openapi.controller;
 
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.MediaType;
 
+import br.com.felipe.gorisfood.api.exceptionhandler.Problema;
 import br.com.felipe.gorisfood.api.v1.model.request.CidadeRequestDTO;
 import br.com.felipe.gorisfood.api.v1.model.response.CidadeResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -17,7 +22,15 @@ public interface CidadeControllerOpenApi {
 	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeResponseDTO> listar();
 
-	@Operation(summary = "Busca uma cidade por Od")
+	@Operation(summary = "Busca uma cidade por Id", 
+				responses = {
+						@ApiResponse(responseCode = "200"),
+						@ApiResponse(responseCode = "400", description = "ID da cidade inválido", 
+							content = @Content(
+										schema = @Schema(implementation = Problema.class), 
+										mediaType = MediaType.APPLICATION_JSON_VALUE)
+						)
+				})
 	CidadeResponseDTO buscar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long id);
 	
 	@Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade, " +
