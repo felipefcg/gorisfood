627a07f636aa6b6df83bff0792454cf778ff053f
Felipe Goriano <Felipe Goriano@host.docker.internal>
2023-09-20 18:28:24 -0300

    26.21. Descrevendo parâmetros de projeções em endpoints de consultas

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
@@ -9,12 +9,25 @@ import br.com.felipe.gorisfood.api.v1.model.request.RestauranteRequestDTO;
 import br.com.felipe.gorisfood.api.v1.model.response.RestauranteApenasNomeResponseDTO;
 import br.com.felipe.gorisfood.api.v1.model.response.RestauranteBasicoResponseDTO;
 import br.com.felipe.gorisfood.api.v1.model.response.RestauranteResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Restaurantes")
 @SecurityRequirement(name = "security_auth")public interface RestauranteControllerOpenApi {
+
+	@Operation(parameters = {
+			@Parameter(name = "projecao",
+					description = "Nome da projeção",
+					example = "apenas-nome",
+					in = ParameterIn.QUERY,
+					required = false)
+	})
 	CollectionModel<RestauranteBasicoResponseDTO> listar();
+	
+	@Operation(hidden = true)
 	CollectionModel<RestauranteApenasNomeResponseDTO> listarApenasNome();
 	RestauranteResponseDTO buscar(Long id);
 	RestauranteResponseDTO criar(RestauranteRequestDTO restauranteDTO);
