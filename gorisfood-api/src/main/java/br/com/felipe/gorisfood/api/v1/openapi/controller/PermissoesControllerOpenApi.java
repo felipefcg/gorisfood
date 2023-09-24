58a71d3ffe751611f7930f218cf470fe0a83f12b
Felipe Goriano <Felipe Goriano@192.168.1.16>
2023-09-24 12:33:01 -0300

    26.35. Desafio: descrevendo documentação de endpoint de permissões

--- gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/PermissoesControllerOpenApi.java
+++ gorisfood-api/src/main/java/br/com/felipe/gorisfood/api/v1/openapi/controller/PermissoesControllerOpenApi.java
@@ -3,10 +3,13 @@ package br.com.felipe.gorisfood.api.v1.openapi.controller;
 import org.springframework.hateoas.CollectionModel;
 
 import br.com.felipe.gorisfood.api.v1.model.response.PermissaoResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Permissões")
 @SecurityRequirement(name = "security_auth")public interface PermissoesControllerOpenApi {
+	
+	@Operation(summary = "Lista as permissões")
 	CollectionModel<PermissaoResponseDTO> listar();
 }
